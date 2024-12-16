7d191cd4cc5761745883ce21de7b6d4c0e83347d
Rob March <robertmarch5@gmail.com>
2024-12-16 09:20:57 +0000

    Minor cleanup to day 16

--- src/main/kotlin/days/Day16.kt
+++ src/main/kotlin/days/Day16.kt
@@ -29,7 +29,7 @@ class Day16 : Day(16) {
         abstract fun getAdjacentDirections(): List<Direction>
     }
 
-    private data class ReachableState(val position: Point2D, val direction: Direction)
+    private data class ReachableStateData(val position: Point2D, val direction: Direction)
 
     private fun parseState(inputString: String): Triple<Point2D, Point2D, Set<Point2D>> {
         val paths: MutableSet<Point2D> = mutableSetOf()
@@ -57,18 +57,18 @@ class Day16 : Day(16) {
     override fun solvePartOne(inputString: String): Long {
         val (start: Point2D, end: Point2D, paths: Set<Point2D>) = parseState(inputString)
 
-        val exploredStates: MutableMap<ReachableState, Long> = mutableMapOf()
-        val nextStates: MutableMap<ReachableState, Long> = mutableMapOf(Pair(ReachableState(start, Direction.EAST), 0))
+        val exploredStates: MutableMap<ReachableStateData, Long> = mutableMapOf()
+        val nextStates: MutableMap<ReachableStateData, Long> = mutableMapOf(Pair(ReachableStateData(start, Direction.EAST), 0))
 
         while (nextStates.isNotEmpty() && !exploredStates.any { it.key.position == end }) {
-            val (nextState: ReachableState, nextDistance: Long) = nextStates.minBy { it.value }
+            val (nextState: ReachableStateData, nextDistance: Long) = nextStates.minBy { it.value }
             exploredStates[nextState] = nextDistance
             nextStates.remove(nextState)
 
-            val possibleNextStates: List<Pair<ReachableState, Long>> = listOf(
-                Pair(ReachableState(nextState.position.addToPoint(nextState.direction.vector), nextState.direction), nextDistance + 1),
-                Pair(ReachableState(nextState.position, nextState.direction.getAdjacentDirections()[0]), nextDistance + 1000),
-                Pair(ReachableState(nextState.position, nextState.direction.getAdjacentDirections()[1]), nextDistance + 1000),
+            val possibleNextStates: List<Pair<ReachableStateData, Long>> = listOf(
+                Pair(ReachableStateData(nextState.position.addToPoint(nextState.direction.vector), nextState.direction), nextDistance + 1),
+                Pair(ReachableStateData(nextState.position, nextState.direction.getAdjacentDirections()[0]), nextDistance + 1000),
+                Pair(ReachableStateData(nextState.position, nextState.direction.getAdjacentDirections()[1]), nextDistance + 1000),
             )
 
             possibleNextStates.filter {
@@ -81,26 +81,31 @@ class Day16 : Day(16) {
         return exploredStates.filterKeys { it.position == end }.values.min()
     }
 
-    private class State(val reachableState: ReachableState, var distance: Long, val visitedPoints: MutableSet<Point2D>) {
+    private class ReachableState(val position: Point2D, val direction: Direction, var distance: Long, val visitedPoints: MutableSet<Point2D>) {
         override fun equals(other: Any?): Boolean {
             if (this === other) return true
             if (javaClass != other?.javaClass) return false
 
-            other as State
+            other as ReachableState
 
-            return reachableState == other.reachableState
+            if (position != other.position) return false
+            if (direction != other.direction) return false
+
+            return true
         }
 
         override fun hashCode(): Int {
-            return reachableState.hashCode()
+            var result = position.hashCode()
+            result = 31 * result + direction.hashCode()
+            return result
         }
     }
 
-    private fun printMapAndBestRoutes(exploredStates: Set<State>, paths: Set<Point2D>, end: Point2D) {
-        val stateToEnd: State = exploredStates.find { it.reachableState.position == end }!!
+    private fun printMapAndBestRoutes(exploredStates: Set<ReachableState>, paths: Set<Point2D>, end: Point2D) {
+        val stateToEnd: ReachableState = exploredStates.find { it.position == end }!!
 
-        for (y in 0..exploredStates.maxOf { it.reachableState.position.y } + 1) {
-            for (x in 0..exploredStates.maxOf { it.reachableState.position.x } + 1) {
+        for (y in 0..exploredStates.maxOf { it.position.y } + 1) {
+            for (x in 0..exploredStates.maxOf { it.position.x } + 1) {
                 val p: Point2D = Point2D(x, y)
                 print(when {
                     stateToEnd.visitedPoints.contains(p) -> 'O'
@@ -115,28 +120,28 @@ class Day16 : Day(16) {
     override fun solvePartTwo(inputString: String): Int {
         val (start: Point2D, end: Point2D, paths: Set<Point2D>) = parseState(inputString)
 
-        val exploredStates: MutableSet<State> = mutableSetOf()
-        val nextStates: MutableSet<State> = mutableSetOf(State(ReachableState(start, Direction.EAST), 0, mutableSetOf(start)))
+        val exploredStates: MutableSet<ReachableState> = mutableSetOf()
+        val nextStates: MutableSet<ReachableState> = mutableSetOf(ReachableState(start, Direction.EAST, 0, mutableSetOf(start)))
 
-        while (nextStates.isNotEmpty() && !exploredStates.any { it.reachableState.position == end }) {
-            val nextState: State = nextStates.minBy { it.distance }
+        while (nextStates.isNotEmpty() && !exploredStates.any { it.position == end }) {
+            val nextState: ReachableState = nextStates.minBy { it.distance }
             exploredStates.add(nextState)
             nextStates.remove(nextState)
 
-            val nextPositionAfterMove: Point2D = nextState.reachableState.position.addToPoint(nextState.reachableState.direction.vector)
+            val nextPositionAfterMove: Point2D = nextState.position.addToPoint(nextState.direction.vector)
             val nextVisitedAfterMove: MutableSet<Point2D> = nextState.visitedPoints.toMutableSet()
             nextVisitedAfterMove.add(nextPositionAfterMove)
 
-            val possibleNextStates: List<State> = listOf(
-                State(ReachableState(nextPositionAfterMove, nextState.reachableState.direction), nextState.distance + 1, nextVisitedAfterMove),
-                State(ReachableState(nextState.reachableState.position, nextState.reachableState.direction.getAdjacentDirections()[0]), nextState.distance + 1000, nextState.visitedPoints),
-                State(ReachableState(nextState.reachableState.position, nextState.reachableState.direction.getAdjacentDirections()[1]), nextState.distance + 1000, nextState.visitedPoints),
+            val possibleNextStates: List<ReachableState> = listOf(
+                ReachableState(nextPositionAfterMove, nextState.direction, nextState.distance + 1, nextVisitedAfterMove),
+                ReachableState(nextState.position, nextState.direction.getAdjacentDirections()[0], nextState.distance + 1000, nextState.visitedPoints),
+                ReachableState(nextState.position, nextState.direction.getAdjacentDirections()[1], nextState.distance + 1000, nextState.visitedPoints),
             )
 
             possibleNextStates.filter {
-                paths.contains(it.reachableState.position) && !exploredStates.contains(it) && (nextStates.find { next -> it == next }?.distance ?: Long.MAX_VALUE) >= it.distance
+                paths.contains(it.position) && !exploredStates.contains(it) && (nextStates.find { next -> it == next }?.distance ?: Long.MAX_VALUE) >= it.distance
             }.forEach {
-                val existingNextState: State? = nextStates.find { next -> it == next }
+                val existingNextState: ReachableState? = nextStates.find { next -> it == next }
                 if (existingNextState?.distance == it.distance) {
                     existingNextState.visitedPoints.addAll(it.visitedPoints)
                 } else {
@@ -147,7 +152,7 @@ class Day16 : Day(16) {
 
 //        printMapAndBestRoutes(exploredStates, paths, end)
 
-        return exploredStates.filter { it.reachableState.position == end }.minBy {it.distance}.visitedPoints.size
+        return exploredStates.filter { it.position == end }.minBy {it.distance}.visitedPoints.size
     }
 }
 
